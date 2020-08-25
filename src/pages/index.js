876b416c6911847c157ad3f98dc82c703c1056bc
emmieworks <filledforest@gmail.com>
2020-08-25 15:38:00 +0900

    update topage add menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,82 @@ import SEO from "../components/SEO"
 import BlogMenu from "../components/BlogMenu"
 import Img from "gatsby-image"
 
-const PageTitle = styled.h1`
+const MenuSection = styled.section`
+  background-color: var(--white);
+
+  padding-top: var(--smallMargin);
+  padding-bottom: var(--smallMargin);
+  margin: var(--middleMargin) auto;
+
+  .flex {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    justify-content: space-around;
+    padding: 2rem auto;
+
+    @media screen and (max-width: 780px) {
+      flex-direction :column;
+      justify-content:center;
+    }
+  }
+  .reverse{
+    flex-direction :row-reverse;
+    @media screen and (max-width: 780px) {
+      flex-direction :column;
+    }
+  }
+  .image{
+    flex: 1;
+    position: relative;
+    figure{
+      max-width: 100%;
+      width: 300px;
+      height: auto;
+      margin: var(--smallMargin) auto;
+    }
+  }
+  .text{
+    flex: 1;
+    max-width: 400px;
+    font-size: 0.8rem;
+    line-height: 2;
+    margin: 30px 0px;
+    padding: 20px;
+  }
+  .service{
+    align-content: flex-end;
+  }
+  .service .image span{
+    font-family: 'allura', cursive;
+    font-size: 4rem;
+    color: var(--secondary);
+    position: absolute;
+    top: 0%;
+    left: 60%;
+    display: block;
+    transform: rotate(15deg);
+  }
+  .about{
+    align-content: center;
+    text-align: right;
+    @media screen and (max-width: 780px) {
+      text-align: center;
+    }
+  }
+  .about .image span{
+    font-family: 'allura', cursive;
+    font-size: 4rem;
+    color: var(--secondary);
+    position: absolute;
+    top: 10%;
+    left: 0%;
+    display: block;
+    transform: rotate(-20deg);
+  }
+`
+
+const SectionTitle = styled.h2`
   text-align: center;
   margin-bottom: 30px;
   position: relative;
@@ -117,8 +192,45 @@ const Index = ({ data, location }) => {
   return (
     <Layout>
       <SEO location = {location} title = "Home" />
-      <div className = "container">
-        <PageTitle>Blog</PageTitle>
+       <MenuSection>
+        <div className="container">
+          <div className = "flex about">
+            <div className = "image">
+              <figure>
+                <Img
+                  fluid = {data.about.childImageSharp.fluid}
+                  alt = "about"
+                />
+              </figure>
+              <span>About</span>
+            </div>
+            <div className = "text">
+            <p>はじめましての方はこちらへ <br />
+            自己紹介と、このサイトについてのご紹介です</p>
+            <Link to = "/about/" className = "btn">about</Link>
+            </div>
+          </div>
+          <div className = "flex reverse service">
+            <div className = "image">
+              <figure>
+                <Img
+                  fluid={data.service.childImageSharp.fluid}
+                  alt="service"
+                />
+              </figure>
+              <span>Sevice</span>
+            </div>
+            <div className = "text">
+              <p>ホームページ制作やWordPressのレクチャーをしています。<br />
+              一人ひとりに合わせたオリジナルデザインで、<br />
+              大切に育てていける場所をお作りします。</p>
+              <Link to = "/service/" className = "btn">service</Link>
+            </div>
+          </div>
+        </div>
+      </MenuSection>
+      <section className = "container">
+        <SectionTitle>Blog</SectionTitle>
         <Description>Web制作／デザインをテーマに発信しています</Description>
         {posts.map(({ node }) => {
           const tags = node.frontmatter.tags
@@ -174,7 +286,7 @@ const Index = ({ data, location }) => {
           )
         })}
         <Link to="/blog/" className="btn">SEE MORE</Link>
-      </div>
+      </section>
       <BlogMenu />
     </Layout>
   )
@@ -183,6 +295,20 @@ export default Index
 
 export const pageQuery = graphql`
   query {
+    service: file(absolutePath: { regex: "/top-service.jpg/" }) {
+      childImageSharp {
+        fluid(maxWidth: 800) {
+            ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    about: file(absolutePath: { regex: "/top-about.jpg/" }) {
+      childImageSharp {
+        fluid(maxWidth: 800) {
+            ...GatsbyImageSharpFluid
+        }
+      }
+    }
     noimage:file(relativePath:{eq: "noimage.jpg"}){
       childImageSharp{
         fluid(maxWidth: 800){
